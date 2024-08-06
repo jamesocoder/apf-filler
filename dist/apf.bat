bcab31c588d42fb32ce479a55bcbb503098187b7
jamesocoder <44654277+jamesocoder@users.noreply.github.com>
2024-08-06 15:57:44 -0400

    Further comment clarification

--- dist/apf.bat
+++ dist/apf.bat
@@ -5,12 +5,14 @@
 :: Can remove need for parameters by hard-coding program path and user's name in instead
 :: Expects the APF REDCap report to be downloaded to the APF directory as apf.csv
 
-:: Open APF program directory
+:: Open APF program directory (1st CLI argument)
 cd %1
 
+:: Execute XML creation with user's name (2nd CLI argument)
 apf-filler.exe apf.csv %2
 
 :: Assumes Adobe Acrobat is the default program for PDFs
+:: Open the APF to allow the user to import the XML into it
 start APF_Original.pdf
 
 cd ../..
