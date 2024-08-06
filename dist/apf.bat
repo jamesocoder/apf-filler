12e63eacb4539544d0e15355c0e2965cc888e91f
jamesocoder <44654277+jamesocoder@users.noreply.github.com>
2024-08-06 10:50:18 -0400

    Update apf.bat
    Comment correction

--- dist/apf.bat
+++ dist/apf.bat
@@ -1,7 +1,7 @@
 :: Shortcut to Academic Profile Form Filler
 :: Version 1.1
 :: Save this file to cmd's default directory (C:\Users\[Username])
-:: Script can then be run from cmd's default directory with command: apf [Full Name]
+:: Script can then be run from cmd's default directory with command: apf [APF_Program_Dir] [Full Name]
 :: Can remove need for parameters by hard-coding program path and user's name in instead
 :: Expects the APF REDCap report to be downloaded to the APF directory as apf.csv
 
